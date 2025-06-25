b8c622624fcdc759c445aa2713929cb52bf8f9b2
Arthur Araujo <112352001+ArthurAraujo05@users.noreply.github.com>
2025-06-25 16:46:28 -0300

    teste

--- forms-tck/components/ game-voting-form.tsx
+++ forms-tck/components/ game-voting-form.tsx
@@ -41,7 +41,7 @@ export default function GameVotingForm() {
     // Estado de busca
     const [searchTerm, setSearchTerm] = useState("")
 
-    const ADMIN_PASSWORD = process.env.PASSWORD // Senha padrão para admin
+    const ADMIN_PASSWORD = process.env.NEXT_PUBLIC_PASSWORD 
 
     // Carregar dados do Supabase
     const loadData = useCallback(async () => {
