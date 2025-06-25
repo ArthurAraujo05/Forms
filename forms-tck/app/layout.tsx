ecdda6ef5c7e8e5d78ce33b551ca911034a4f431
Arthur Araujo <112352001+ArthurAraujo05@users.noreply.github.com>
2025-06-25 14:03:02 -0300

    analytics

--- forms-tck/app/layout.tsx
+++ forms-tck/app/layout.tsx
@@ -1,5 +1,6 @@
 import type React from "react"
 import type { Metadata } from "next"
+import { Analytics } from '@vercel/analytics/next';
 import { Inter } from "next/font/google"
 import "./globals.css"
 
@@ -17,7 +18,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="pt-BR">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>{children}
+        <Analytics />
+      </body>
     </html>
   )
 }
